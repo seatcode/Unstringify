83e4271981138fe6a9488f3650d9ca00402e55c9
hectr <h@mrhector.me[core]>
2019-10-04 16:03:42 +0200

    Unit test String extensions

--- Sources/UnstringifyFramework/Foundation/String+localizableKey.swift
+++ Sources/UnstringifyFramework/Foundation/String+localizableKey.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension String {
     public var localizableKey: String? {
-        // "x" = "y"; -> "x -> x
+        // '"x" = "y";' -> '"x' -> 'x'
         return components(separatedBy: "\" = \"").first?.removingCharacter(at: 0)
     }
 }
--- Tests/UnstringifyFrameworkTests/UnstringifyFrameworkTests.swift
+++ Tests/UnstringifyFrameworkTests/UnstringifyFrameworkTests.swift
@@ -2,11 +2,77 @@ import XCTest
 import UnstringifyFramework
 
 final class UnstringifyFrameworkTests: XCTestCase {
-    func test() {
-        // TODO: @hectr
+    func testParseKeys() throws {
+        let content = """
+// COMMENT
+
+/*
+ another
+ comment
+*/
+
+"key0" = "value0";
+"key1" = "value1";
+
+"""
+        let keys = try parseKeys(content)
+        XCTAssertEqual(Set(keys), Set(["key0", "key1"]))
+    }
+
+    func testContainsHTML() throws {
+        XCTAssertNoThrow(try "".containsHTML())
+        XCTAssertFalse(try "<b>".containsHTML())
+        XCTAssertFalse(try "<strong>".containsHTML())
+        XCTAssertFalse(try "</em>".containsHTML())
+        XCTAssertTrue(try "<b></b>".containsHTML())
+        XCTAssertTrue(try "<strong>Hello</strong>".containsHTML())
+        XCTAssertTrue(try "<em>1</em>".containsHTML())
+
+    }
+
+    func testFormatSpecifiers() throws {
+        let result = try "ABC %d (%@,%f) %s%3$s 123XXX".formatSpecifiers()
+        let expected = ["%d", "%@", "%f", "%s", "%3$s"]
+        XCTAssertEqual(result, expected)
+    }
+
+    func testIsINfoPlistValueLine() {
+        XCTAssertTrue("\"infoplist_XXX\"= \"\";".isInfoPlistValueLine)
+        XCTAssertFalse("\"info_plist_XXX\"= \"\";".isInfoPlistValueLine)
+    }
+
+    func testIsKeyValueLine() {
+        XCTAssertTrue("\"x\" = \"y\";".isKeyValueLine)
+        XCTAssertFalse(" \"x\" = \"y\";".isKeyValueLine)
+        XCTAssertFalse("//\"x\" = \"y\";".isKeyValueLine)
+    }
+
+    func testLocalizableKey() {
+        let result = "\"x\" = \"y\";".localizableKey
+        let expected = "x"
+        XCTAssertEqual(result, expected)
+    }
+
+    func testMatches() throws {
+        let result = try "1.1".matches(regex: "[:alnum:]")
+        let expected = ["1", "1"]
+        XCTAssertEqual(result, expected)
+    }
+
+    func testRemovingCharacterAt() {
+        let result = "ABC".removingCharacter(at: 1)
+        let expected = "AC"
+        XCTAssertEqual(result, expected)
     }
 
     static var allTests = [
-        ("test", test),
+        ("testParseKeys", testParseKeys),
+        ("testContainsHTML", testContainsHTML),
+        ("testFormatSpecifiers", testFormatSpecifiers),
+        ("testIsINfoPlistValueLine", testIsINfoPlistValueLine),
+        ("testIsKeyValueLine", testIsKeyValueLine),
+        ("testLocalizableKey", testLocalizableKey),
+        ("testMatches", testMatches),
+        ("testRemovingCharacterAt", testRemovingCharacterAt),
     ]
 }
