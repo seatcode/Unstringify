52f0d301e0b63c2a267becb619dab253d54df576
hectr <h@mrhector.me[core]>
2019-10-18 03:13:09 +0200

    Add modularised example project

---
+++ Examples/CocoaPods+Module/ModuleExampleTests/ModuleExampleTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import ModuleExample
+
+class ModuleExampleTests: XCTestCase {
+    func test() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateInitialViewController() as? ViewController
+        _ = controller?.view
+        XCTAssertEqual(controller?.formTitle?.text, "form_title")
+        XCTAssertEqual(controller?.fieldName?.text, "form_name_field")
+        XCTAssertEqual(controller?.fieldDescription?.text, "form_name_field_description")
+        XCTAssertEqual(controller?.fieldLimit?.text, "form_name_field_max_length")
+    }
+}
