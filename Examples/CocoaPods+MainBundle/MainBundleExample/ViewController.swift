6e5ae8af4c30efe37864335cd9a113169756a036
hectr <h@mrhector.me[core]>
2019-10-18 02:36:08 +0200

    Add example project

---
+++ Examples/CocoaPods+MainBundle/MainBundleExample/ViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var formTitle: UILabel?
+    @IBOutlet weak var fieldName: UILabel?
+    @IBOutlet weak var fieldDescription: UILabel?
+    @IBOutlet weak var fieldLimit: UILabel?
+    @IBOutlet weak var input: UITextField?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        formTitle?.text = Text.form_title.string
+        fieldName?.text = Text.form_name_field.string
+        fieldDescription?.attributedText = RichText.form_name_field_description.string
+        fieldLimit?.text = Format.form_name_field_max_length(100).string
+    }
+}
