24f197e85172cb9c51a59be95b164e4ee3186635
Carlos Solana Martínez <carlos.solana.martinez@gmail.com>
2020-04-14 17:58:51 +0200

    Remove failing assert for format with percentage symbol

--- Sources/Unstringified/Internal/Unstringified+plainString.swift
+++ Sources/Unstringified/Internal/Unstringified+plainString.swift
@@ -4,7 +4,6 @@ extension Unstringified where Self: RawRepresentable, Self.RawValue == String {
     var plainString: String {
         let string = localizedString
         assert(Mirror(reflecting: self).children.first?.label == nil, "Use `formattedString` for parameterized strings.")
-        // assert(String(format: string) == string, "'\(key)' contains format arguments. Use `format(...)` instead of `string`.")
         return string
     }
 
